451bb3a386b967f5008b8dee39aa138b577b604e
Hadi Faraz <hadifaraz52@protonmail.com>
2025-11-08 18:48:56 -0700

    Fix app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const jwt = require('jsonwebtoken');
-const express = require('express');
+const app = require('express');
 const { Pool } = require('pg');
 const bcrypt = require('bcryptjs');
 
